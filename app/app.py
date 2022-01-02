2c93de369f38c59f9985e413a550ae263284cc3f
Um Chnagyong <e7217@naver.com>
2022-01-02 23:36:03 +0900

    added abc

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import bs4
 
+from abc import *
+
 
 # res = requests.get("https://blog.naver.com/SympathyHistoryList.naver?blogId=wjdalsry277&logNo=222190961111&layoutWidthClassName=contw-966&currentPage=1")
 
@@ -18,7 +20,7 @@ class User:
     def link(self):
         return "https://blog.naver.com/" + self.id
 
-class Notice:
+class Notice(metaclass = ABCMeta):
     def __init__(self, url):
         self.url = url
         pass
@@ -26,7 +28,23 @@ class Notice:
     def info(self):
         pass
 
+    @abstractmethod
     def get_bloger(self, tag: bs4.element.Tag):
+        pass
+
+    @abstractmethod
+    def like(self):
+        pass
+
+
+    @abstractmethod
+    def _get_liked_bloger(self):
+        pass
+
+class Blog(Notice):
+
+    def get_bloger(self, tag: bs4.element.Tag):
+        # return super().get_bloger(tag)
         try:
             name = tag.find("span", attrs={"class":"ell2"}).a.text
             id = tag.find("span", attrs={"class":"ell2"}).a["href"].split("/")[-1]
@@ -37,7 +55,8 @@ class Notice:
             
         return User(name, id)
 
-    def liked_bloger(self):
+    def like(self):
+        # return super().like()
         res = requests.get(self.url)
         html = BeautifulSoup(res.text, 'html.parser')
         liked_bloger = html.find_all('div', {'class':'bloger'})
@@ -46,8 +65,10 @@ class Notice:
             _bloger.append(self.get_bloger(bloger))
         return _bloger
 
-    def _get_liked_bloger(self):
-        pass
+    ...
+
+class Post(Notice):
+    ...
 
 class Bloger(User):
     def __init__(self):
@@ -63,7 +84,7 @@ for i in range(1, 10):
     print("i : ", i)
     target = Notice(f"https://blog.naver.com/SympathyHistoryList.naver?blogId=wjdalsry277&logNo=222190961111&layoutWidthClassName=contw-966&currentPage={i}")
 
-    blogers = target.liked_bloger()
+    blogers = target.like()
 
     for key, bloger in enumerate(blogers):
         print(f"[{key}] bloger - name : {bloger.name}, id : {bloger.id}")
